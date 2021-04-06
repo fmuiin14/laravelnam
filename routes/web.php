70cc5250a0206510f4532177961f60b36b96ddf7
Fathul Muiin <fmuiin14@gmail.com>
2021-04-06 22:45:02 +0700

    1 fresh spartie with roles

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('admin-page', function() {
+    return 'Halaman untuk Admin';
+})->middleware('role:admin')->name('admin.page');
+
+Route::get('user-page', function() {
+    return 'Halaman untuk User';
+})->middleware('role:finance')->name('finance.page');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
